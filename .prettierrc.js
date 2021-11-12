3828786fb59c947fc094961ddab74fa12bb0fd2b
shugetsu <1968304304@qq.com>
2021-11-12 14:49:15 +0800

    fix(prettierrc): 修复标签格式化后显示异常的问题

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 /**
  * @date 2021-10-11 19:05:27
- * @lastEditTime 2021-10-11 19:09:38
+ * @lastEditTime 2021-11-12 14:48:58
  * @description prettier 配置
  * @filePath /.prettierrc.js
  * @see https://prettier.io/docs/en/options.html
@@ -14,6 +14,6 @@ module.exports = {
   trailingComma: 'none', // 去除对象最末尾元素跟随的逗号
   arrowParens: 'always', // 箭头函数，只有一个参数的时候，也需要括号
   vueIndentScriptAndStyle: true, // 是否缩进Vue 文件中的代码<script>和<style>标签。有些人（如Vue 的创建者）不缩进以保存缩进级别，但这可能会破坏编辑器中的代码折叠。
-  htmlWhitespaceSensitivity: 'strict', // 全局空白敏感度
+  htmlWhitespaceSensitivity: 'ignore', // 全局空白敏感度
   proseWrap: 'always' // 当超出print width（上面有这个参数）时就折行
 }
